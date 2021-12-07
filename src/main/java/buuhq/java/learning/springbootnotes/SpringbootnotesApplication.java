4b0d23e2de370aeab527bf4bcbe4c5ae932cd063
huynhquocbuu <huynhquocbuu@gmail.com>
2021-12-07 09:54:55 +0700

    ddd

---
+++ src/main/java/buuhq/java/learning/springbootnotes/SpringbootnotesApplication.java
@@ -0,0 +1,13 @@
+package buuhq.java.learning.springbootnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootnotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootnotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/buuhq/java/learning/springbootnotes/SpringbootnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package buuhq.java.learning.springbootnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootnotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
